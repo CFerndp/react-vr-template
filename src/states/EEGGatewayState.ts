acc129679d91259847e73af56781e6beca47f77a
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2024-07-12 19:51:18 +0200

    New Ip

--- src/states/EEGGatewayState.ts
+++ src/states/EEGGatewayState.ts
@@ -5,6 +5,6 @@ import { atom } from "recoil";
 
 export const EEGGatewayAtom = atom<EEGGateway>({
   key: "eegGateway", // unique ID (with respect to other atoms/selectors)
-  default: new RESTGateway("192.168.56.1", "8000"), // default value (aka initial value)
+  default: new RESTGateway("192.168.245.141", "8000"), // default value (aka initial value)
   // default: new TestGateway(),
 });
