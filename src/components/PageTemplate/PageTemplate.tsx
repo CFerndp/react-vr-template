f4ed78b8d5deceee8d9b68b0e098804b2ac57d05
Cristian Fernandez <cristian.fernandez.del.pozo@gmail.com>
2024-07-23 16:08:53 +0200

    Added scroll

--- src/components/PageTemplate/PageTemplate.tsx
+++ src/components/PageTemplate/PageTemplate.tsx
@@ -10,7 +10,7 @@ const PageTemplate: React.FC<PageTemplateProps> = ({
     <main
       className={`dark bg-gradient-to-b from-slate-900 via-slate-500 to-slate-900 text-foreground bg-background w-screen h-screen ${
         fullScreen ? "p-4" : "p-0"
-      }`}
+      } scroll-smooth scrollbar-thin scrollbar-thumb-gray-700 scrollbar-track-gray-800 overflow-auto`}
     >
       {children}
     </main>
